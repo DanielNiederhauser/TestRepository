ba321e1f0bf7a071957cdb4ae75014aaf674cb58
daniel.niederh@gmail.com <daniel.niederh@gmail.com>
2020-01-30 12:49:35 +0100

    Interface RepositoryArticle erzeugt

---
+++ src/RepositoryArticles.java
@@ -0,0 +1,17 @@
+import java.util.List;
+public interface RepositoryArticles {
+    //in einem Interface gibt man alle Methoden an welche von
+    // implementierenden Klassen ausprogrammiert werden müssen
+    //WICHTIG:
+    //interfaces geben einen Vertrag vor
+    //es werden nur die Methodenköpfe angegeben
+    //      es existiert noch kein Code
+    //      diesen müssen die implementierenden Klassen bereitstellen
+    public boolean Save();
+    public boolean insert(Article article);
+    public boolean remove(int primaryKey);
+    public boolean update(int primaryKey, Article newArticleData);
+    public List<Article> SearchByName(String name);
+    public List<Article> SearchByCategory (String category);
+    public Article SearchById(int primaryKey);
+}
